5592c1db026e9eeec35f09f4d874c5d10b750c26
Namyts <James-Styman@hotmail.co.uk>
2019-03-15 16:22:28 +0000

    Made GenericDropdown CSS styles configurable, as well as adding defaults

--- table/src/dropdownCell/GenericDropdown.js
+++ table/src/dropdownCell/GenericDropdown.js
@@ -3,11 +3,12 @@ import './DropdownCell.css';
 
 export default class DropdownCellEditor extends Component {
   render() {
+	const style = this.props.style || {}
     return (
-      <div className="dropdown" style={this.props.style} onBlur={this.props.onBlur}>
-        {this.props.canSearch && <input className="dropdown-input" autoFocus type="text" value={this.props.searchString} onChange={e => this.props.onSearchChange(e.target.value)} placeholder={this.props.placeholder || "Search.."} />}
-        <ul className="dropdown-menu">
-          {Object.keys(this.props.options).map((v,index) => <li className="dropdown-item" autoFocus key={index} onMouseDown={() => this.props.submit(v)}>{this.props.options[v]}</li>)}
+      <div className={style.dropdown || "dropdown"} style={style} onBlur={this.props.onBlur}>
+        {this.props.canSearch && <input className={style.dropdownInput || "dropdown-input"} autoFocus type="text" value={this.props.searchString} onChange={e => this.props.onSearchChange(e.target.value)} placeholder={this.props.placeholder || "Search.."} />}
+        <ul className={style.dropdownMenu ||"dropdown-menu"}>
+          {Object.keys(this.props.options).map((v,index) => <li className={style.dropdownItem ||"dropdown-item"} autoFocus key={index} onMouseDown={() => this.props.submit(v)}>{this.props.options[v]}</li>)}
         </ul>
       </div>);
   }
