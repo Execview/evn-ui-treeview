b6b430435c2c1aef3485d4fb97b7f93e1ed84927
Andras Ludanyi <andras.ludanyi@execview.com>
2019-04-03 14:24:20 +0100

    npm publish pass fix registry 2

--- build/build.sh
+++ build/build.sh
@@ -5,7 +5,7 @@ VER=`date +%y.%m.%d.%H%m%S`
 npm set registry https://registry.npmjs.org/
 npm install -g npm-cli-login
 npm set registry https://npm.evlem.net
-npm-cli-login -u jenkins -p $1 -e $2
+NPM_REGISTRY=https://npm.evlem.net NPM_USER=jenkins NPM_PASS=$1 NPM_EMAIL=$2 npm-cli-login
 
 ./../menu/npm publish
 ./../table/npm publish
