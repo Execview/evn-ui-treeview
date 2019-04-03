1d82d2caf99309b8414820cd2f258cb955f4770b
Andras Ludanyi <andras.ludanyi@execview.com>
2019-04-03 13:48:00 +0100

    npm publish pass fix

--- build/build.sh
+++ build/build.sh
@@ -2,13 +2,13 @@ NAME="evn-ui-lib"
 GIT_HASH=$(git log -1 --format="%H")
 VER=`date +%y.%m.%d.%H%m%S`
 
-../npm set registry https://npm.evlem.net
+npm set registry https://npm.evlem.net
 
 npm install -g npm-cli-login
 
 npm-cli-login -u jenkins -p $1 -e 'andras.ludanyi@execview.com'
 
-../menu/npm publish
-../table/npm publish
-../treeview/npm publish
-../scheduler/npm publish
+./../menu/npm publish
+./../table/npm publish
+./../treeview/npm publish
+./../scheduler/npm publish
