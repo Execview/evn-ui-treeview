af74d2a6e491ffc5ffee564e381e048fab94a440
Andras Ludanyi <andras.ludanyi@execview.com>
2019-04-03 14:01:18 +0100

    npm publish pass fix registry

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,7 @@ NAME="evn-ui-lib"
 GIT_HASH=$(git log -1 --format="%H")
 VER=`date +%y.%m.%d.%H%m%S`
 
+npm set registry https://registry.npmjs.org/
 npm install -g npm-cli-login
 npm set registry https://npm.evlem.net
 npm-cli-login -u jenkins -p $1 -e $2
@@ -10,3 +11,5 @@ npm-cli-login -u jenkins -p $1 -e $2
 ./../table/npm publish
 ./../treeview/npm publish
 ./../scheduler/npm publish
+
+npm set registry https://registry.npmjs.org/
