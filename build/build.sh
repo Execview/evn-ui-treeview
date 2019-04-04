a1bc6344a003434b5adc4f93883e82695c8d81a3
Andras Ludanyi <andras.ludanyi@execview.com>
2019-04-04 09:51:14 +0100

    fix?

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@ NAME="evn-ui-lib"
 GIT_HASH=$(git log -1 --format="%H")
 VER=`date +%y.%m.%d.%H%m%S`
 
-cd ../menu && npm publish
+cd menu && npm publish
 cd ../table && npm publish
 cd ../treeview && npm publish
 cd ../scheduler && npm publish
