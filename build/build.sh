ffbc2ba3216c7c5030cf67fc58c991164e034129
Andras Ludanyi <andras.ludanyi@execview.com>
2019-04-18 15:56:43 +0100

    npm publish execview private

--- build/build.sh
+++ build/build.sh
@@ -2,7 +2,7 @@ NAME="evn-ui-lib"
 GIT_HASH=$(git log -1 --format="%H")
 VER=`date +%y.%m.%d.%H%m%S`
 
-cd menu && npm publish
-cd ../table && npm publish
-cd ../treeview && npm publish
-cd ../scheduler && npm publish
+cd menu && npm publish --access restricted
+cd ../table && npm publish --access restricted
+cd ../treeview && npm publish --access restricted
+cd ../scheduler && npm publish --access restricted
