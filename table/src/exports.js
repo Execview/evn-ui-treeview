59eaca67dabc066b427d58a9bacee98cf5aac4f8
Andrei <jalba.andrei94@gmail.com>
2019-04-24 12:41:16 +0100

    Enable npm package exports.

---
+++ table/src/exports.js
@@ -0,0 +1,35 @@
+import Table from './table/Table';
+import Cell from './cell/Cell';
+import cats from './store/ElCatso';
+import GenericDropdown from './dropdownCell/GenericDropdown';
+import DropdownCellEditor from './dropdownCell/DropdownCellEditor';
+import InputCellDisplay from './inputCell/InputCellDisplay';
+import InputCellEditor from './inputCell/InputCellEditor';
+import ColorCellDisplay from './colorCell/ColorCellDisplay';
+import ColorCellEditor from './colorCell/ColorCellEditor';
+import DateCellDisplay from './dateCell/DateCellDisplay';
+import DateCellEditor from './dateCell/DateCellEditor';
+import NumberCellEditor from './numberCell/NumberCellEditor';
+import TripleFill from './CircleUser/TripleFill';
+import CircleUserDisplay from './CircleUser/CircleUserDisplay';
+import CircleUser from './CircleUser/CircleUser';
+import UserHeaderDisplay from './headers/UserHeaderDisplay';
+
+module.exports = { 
+  Table,
+  Cell,
+  cats,
+  DropdownCellEditor,
+  GenericDropdown,
+  InputCellDisplay, 
+  InputCellEditor,
+  ColorCellDisplay,
+  ColorCellEditor,
+  DateCellDisplay,
+  DateCellEditor,
+  NumberCellEditor,
+  TripleFill,
+  CircleUser,
+  CircleUserDisplay,
+  UserHeaderDisplay
+};
