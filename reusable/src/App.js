743a26acf99a7f5a240f5148d1d9f312b7a92b3c
Namyts <James-Styman@hotmail.co.uk>
2019-05-13 12:37:01 +0100

    Created reusable folder, which will contain code which should be reused across components rather than copying such as: buttons, functions, dropdown... etc...

---
+++ reusable/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Button from './Components/Button/Button'
+
+function App() {
+  return (
+    <div className="App">
+		<Button>Testing</Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ reusable/src/exports.js
@@ -0,0 +1,5 @@
+import Button from './Components/Button/Button'
+
+module.exports = {
+	Button
+};
