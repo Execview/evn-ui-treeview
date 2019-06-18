fdcf800fcb67f75cd2285e20bf5f155b0f524651
Namyts <James-Styman@hotmail.co.uk>
2019-06-18 14:08:07 +0100

    UserRoleDisplay now uses ImageDisplay

--- table/src/UserRoleDisplay/UserRoleDisplay.js
+++ table/src/UserRoleDisplay/UserRoleDisplay.js
@@ -1,50 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './CircleUser.css';
-import { CircleUser } from '@execview/reusable';
 import AssignUsers from './AssignUsers';
+import ImageDisplay from '../imageDisplay/ImageDisplay'
 
-export default class UserRoleDisplay extends Component {
-  state = { open: false };
+const UserRoleDisplay = (props) => {
+	const [open, setOpen] = useState(false)
 
-  getUserProfile = (user) => {
-    return this.props.userProfiles[user];
-  }
+	const getUserProfile = (user) => {
+		return props.userProfiles[user];
+	}
 
-  getAllUserProfileKeys = () => {
-    return Object.keys(this.props.userProfiles);
-  }
-
-
-  render() {
-    let circlesLimit = this.props.data.length;
-    let addAmount = null;
-    if (this.props.style.width - 10 < (this.props.data.length + 1) * 20) {
-      circlesLimit = parseInt((this.props.style.width - 50) / 20, 10);
-      addAmount = <div className="add-container" style={{ left: ((circlesLimit + 1) * 20) + 1 }}>{'+' + (this.props.data.length - circlesLimit)}</div>;
-    }
-    return (
-      <div className="user-cell">
-        <div className="users-container" onClick={() => { this.setState({ open: !this.state.open }); }}>
-          {this.props.data && this.props.data.slice(0, circlesLimit).map((assigneduser, index) => {
-            return (
-              <div className="user-profile" key={'circle' + (index + 1)} style={{ zIndex: this.props.data.length - index, left: 20 * index }}>
-                <CircleUser url={this.getUserProfile(assigneduser.user).image} />
-              </div>
-            );
-          })}
-          {addAmount}
-        </div>
-        {this.state.open && (
-          <AssignUsers
-            assignedUsers={this.props.data}
-            getUserProfile={this.getUserProfile}
-            getAllUserProfileKeys={this.getAllUserProfileKeys}
-            style={this.props.style}
-            closeMenu={() => this.setState({ open: false })}
-            onValidateSave={this.props.onValidateSave}
-          />
-        )}
-      </div>
-    );
-  }
+	const getAllUserProfileKeys = () => {
+		return Object.keys(props.userProfiles);
+	}
+	const data = props.data || []
+	console.log(data)
+	const userImages = data.map(d => getUserProfile(d.user).image)
+	return (
+		<div className="user-cell" onClick={() => setOpen(true)}>
+			<ImageDisplay data={userImages} style={props.style} />
+			{open && (
+				<AssignUsers
+					assignedUsers={data}
+					getUserProfile={getUserProfile}
+					getAllUserProfileKeys={getAllUserProfileKeys}
+					style={props.style}
+					closeMenu={() => setOpen(false)}
+					onValidateSave={props.onValidateSave}
+				/>
+			)}
+		</div>
+	);
 }
+export default UserRoleDisplay
