bdefc10a25a6b6a0cc4edaf871816d12e110537a
Namyts <James-Styman@hotmail.co.uk>
2019-07-02 12:40:38 +0100

    Removed unneeded reference to css in InPlaceCell

--- reusable/src/Components/InPlaceCell/InPlaceCell.js
+++ reusable/src/Components/InPlaceCell/InPlaceCell.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react'
 import { Cell } from '@execview/table'
-import classes from './Profile.module.css';
 
 const InPlaceCell = (props) => {
 	useEffect(()=>{
