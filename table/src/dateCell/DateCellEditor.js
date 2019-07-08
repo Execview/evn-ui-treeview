5c9d0e50e780ed08e614cb1a38d16f58dfdfddb6
Andrei <jalba.andrei94@gmail.com>
2019-07-08 12:38:30 +0100

    Improved dateCell to receive props for the date picker.

--- table/src/dateCell/DateCellEditor.js
+++ table/src/dateCell/DateCellEditor.js
@@ -1,34 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DatePicker from 'react-datepicker';
 import { isMobile } from 'react-device-detect';
 import 'react-datepicker/dist/react-datepicker.css';
 import './DateCell.css';
 
-export default class DateCellEditor extends Component {
-	submit = (date) => {
-		let saveDate = date || this.props.data;
+const DateCellEditor = (props) => {
+	const submit = (date) => {
+		let saveDate = date || props.data;
 		saveDate = saveDate && (Object.prototype.toString.apply(saveDate) === '[object Date]' ? saveDate : saveDate.toISOString());
-		this.props.onValidateSave(saveDate);
-	}
+		props.onValidateSave(saveDate);
+	};
 
-	getDefaultDate = (date) => {
-		return date.toISOString().split('T')[0];
-	}
+	const selectedDate = props.data ? new Date(props.data) : new Date();
+	const extraDatePickerProps = props.datepickerProps || {};
+	const datePickerProps = {
+		onClickOutside: (() => submit()),
+		selected: selectedDate,
+		onSelect: (date => submit(date)),
+	};
 
-	render() {
-		const selectedDate = this.props.data ? new Date(this.props.data) : new Date();
-		return (
-			<div className="text-container" style={this.props.style}>
-				{!isMobile
-					? <DatePicker onClickOutside={() => this.submit()} selected={selectedDate} onSelect={date => this.submit(date)} autoFocus />
-					: <DatePicker onClickOutside={() => this.submit()} selected={selectedDate} onSelect={date => this.submit(date)} inline withPortal />}
-			</div>
-		);
-	}
-}
+	return (
+		<div className="text-container" style={props.style}>
+			{!isMobile
+				? <DatePicker autoFocus {...datePickerProps} {...extraDatePickerProps} />
+				: <DatePicker inline withPortal {...datePickerProps} />}
+		</div>
+	);
+};
 
-// <input type="date" name="bday" autoFocus defaultValue={this.getDefaultDate(selectedDate)} onChange={e => this.submit(new Date(e.target.value))} onSubmit={()=>{console.log("test")}} />
-// onBlur={() => this.props.onValidateSave(saveDate)}
-// <input autoFocus className="date-input" type="date" name="bday" value={selectedDate} onChange={e => this.props.onValidateSave(new Date(e.target.value).toISOString())} />
-
-// <DatePicker autoFocus onClickOutside={() => this.props.onValidateSave(saveDate)} selected={selectedDate} onSelect={date => this.props.onValidateSave(date.toISOString())} />
+export default DateCellEditor;
